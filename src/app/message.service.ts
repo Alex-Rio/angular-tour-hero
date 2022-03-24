a244cfbb7403ffab569ed987cf76cbe64becff8f
Alex-Rio <80269140+Alex-Rio@users.noreply.github.com>
2022-03-24 09:16:38 +0700

    Frist

---
+++ src/app/message.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HEROS } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages: string[] = [];
+
+
+  // tslint:disable-next-line:typedef
+  add(message: string){
+    this.messages.push(message);
+  }
+
+  // tslint:disable-next-line:typedef
+  clear(){
+    this.messages = [] ;
+  }
+
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero'
+export const HEROS : Hero[]=[
+  { id : 11, name : 'Dr Nice' },
+  { id : 12, name : 'Narco' },
+  { id : 13, name : 'Bombarto' },
+  { id : 14, name : 'Celeristas' },
+  { id : 15, name : 'Mangenta' },
+  { id : 16, name : 'RunbberMan' },
+  { id : 17, name : 'Dynama' },
+  { id : 18, name : 'Dr IQ' },
+  { id : 19, name : 'Magma' },
+  { id : 20, name : 'Tornado' },
+];
+
